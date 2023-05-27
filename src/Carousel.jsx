e9fb06ca16e4704e192443d9477a652695e6ec99
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-27 13:06:02 +0400

    adding slider

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -5,17 +5,36 @@ import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
-  const [people, setPeople] = useState(shortList);
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const [people, setPeople] = useState(longList);
+  const [currentPerson, setCurrentPerson] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
 
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, index) => {
         const { id, image, name, title, quote } = person;
-
         return (
-          <article className="slide" key={id}>
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (index - currentPerson)}%)`,
+              opacity: index === currentPerson ? 1 : 0,
+              visibility: index === currentPerson ? "visible" : "hidden",
+            }}
+            key={id}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
